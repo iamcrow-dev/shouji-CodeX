0500bfb1af6263460fc43b222a8f12464c7b0d7a
iamcrow-dev <iamcrow@gmail.com>
2026-04-15 04:53:20 +0800

    Delete winCodeX directory

--- winCodeX/src/close-confirm-preload.cjs
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("closeDialog", {
-  choose: (action) => ipcRenderer.invoke("close-dialog:choose", action)
-});
